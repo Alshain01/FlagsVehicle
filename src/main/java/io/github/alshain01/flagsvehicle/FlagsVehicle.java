34f2c85e081bf0c12a5b63a28629b8c567573207
Kevin Seiden <alshain01@suddenlink.net>
2014-04-10 07:56:56 -0500

    Update to Flag v2 API

--- src/main/java/io/github/alshain01/flagsvehicle/FlagsVehicle.java
+++ src/main/java/io/github/alshain01/flagsvehicle/FlagsVehicle.java
@@ -39,9 +39,9 @@ import org.bukkit.event.vehicle.VehicleDamageEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Flags Vehicle - Module that adds vehicle flags to the plug-in Flags.
@@ -63,7 +63,7 @@ public class FlagsVehicle extends JavaPlugin {
 
 		// Connect to the data file and register the flags
         YamlConfiguration flagConfig = YamlConfiguration.loadConfiguration(getResource("flags.yml"));
-        Set<Flag> flags = FlagsAPI.getRegistrar().register(flagConfig, "Vehicle");
+        Collection<Flag> flags = FlagsAPI.getRegistrar().registerFlag(flagConfig, "Vehicle");
         Map<String, Flag> flagMap = new HashMap<String, Flag>();
         for(Flag f : flags) {
             flagMap.put(f.getName(), f);
